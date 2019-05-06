44856be753683c2f672557c4c3be48497ee448d1
Henner R Setyono <henner.renardi@gmail.com>
2019-05-06 10:38:13 +0700

    2.2.0
    GUTENBERG
    - Added filter for `the_content` that replace Read More block with button.
    - Added JS to set default alignment.
    - Added JS to add Style in Columns block.
    
    CSS
    - Added custom CSS so the edito fits the styling from Edje WP Theme
    
    JS
    - Added script to toggle Read More block.

--- assets/js/h-library.js
+++ assets/js/h-library.js
@@ -1,18 +1,108 @@
-/*
-  Sharedaddy
-*/
+/**
+ * Gutenberg helper script
+ */
+(function() { 'use strict';
+document.addEventListener( 'DOMContentLoaded', onReady );
+
+function onReady() {
+  gutenbergHelper.init();
+  commentFormToggle();
+}
+
+/////
+
+let gutenbergHelper = {
+  init() {
+    this.faqBlock();
+    this.moreBlock();
+  },
+
+  /**
+   * Toggle the Pullquote when clicking the Citation
+   * Note: Pullquote Block is transformed into FAQ Block
+   */
+  faqBlock() {
+    let $faqs = document.querySelectorAll( '.wp-block-pullquote cite' );
+    for( let $f of $faqs ) {
+      $f.addEventListener( 'click', onClick );
+    }
+
+    //
+    function onClick( e ) {
+      let $faqWrapper = e.currentTarget.closest( '.wp-block-pullquote' );
+      $faqWrapper.classList.toggle( '--expanded' );
+    }
+  },
+
+  /**
+   * Show the content after Read More block
+   */
+  moreBlock() {
+    let $moreButton = document.querySelector( '.h-block-readmore .button' );
+    if( $moreButton ) {
+      $moreButton.addEventListener( 'click', onClick );
+    }
+
+    //
+    function onClick( e ) {
+      e.currentTarget.closest( '.h-block-readmore' ).classList.add( '--expanded' );
+    }
+  }
+}
+
+
+/**
+ * Open Comment Form and add Placeholder to the textarea
+ */
+function commentFormToggle() {
+  // exit if comment form not found
+  if( document.querySelector('#reply-title') === null ) { return; }
+
+  // Create placeholder's text based on Form Title.
+  let replyTitle = document.querySelector( '#reply-title' ).childNodes;
+  let replyTo = replyTitle[1].childNodes[0].nodeValue;
+  let placeholder = replyTitle[0].nodeValue + (replyTo ? replyTo : '') + '…';
+
+  // change the comment field's placeholder
+  document.querySelector('.comment-form textarea').setAttribute( 'placeholder', placeholder );
+  
+  // click listener to toggle the form
+  document.querySelector('.comment-form').addEventListener( 'click', activateForm );
+  
+  let $replyLinks = document.querySelectorAll( '.comment-reply-link' );
+  for( let $rl of $replyLinks ) {
+    $rl.addEventListener( 'click', activateForm );
+  }
+
+  //
+  function activateForm( e ) {
+    var $form = document.querySelector( '.comment-form' );
+    $form.classList.add( '--active' );
+    $form.removeEventListener( 'click', activateForm );
+    $form.querySelector( 'textarea' ).focus();
+  }
+}
+
+})();
+
+
+/**
+ * Jetpack Sharedaddy
+ */
 (function() { 'use strict';
 
   document.addEventListener( 'DOMContentLoaded', onReady );
 
   function onReady() {
+    if( document.querySelector('.sharedaddy') == null ) { return; }
+
     initMoreButton();
     initPrintButton();
   }
 
-  /*
-    Move the hidden sharing buttons to main list when MORE is clicked
-  */
+  /**
+   * Move the hidden sharing buttons to main list when MORE is clicked
+   */
   function initMoreButton() {
     let $moreButtons = document.querySelectorAll( '.share-more' );
     for( let $mb of $moreButtons ) {
@@ -36,9 +126,9 @@
     }
   }
 
-  /*
-    Print the website when Sharedaddy's print button is clicked
-  */
+  /**
+   * Print the website when Sharedaddy's print button is clicked
+   */
   function initPrintButton() {
     let $printButtons = document.querySelectorAll( '.sd-content .share-print' );
     for( let $pb of $printButtons ) {
@@ -46,47 +136,6 @@
         e.preventDefault(); e.stopPropagation();
         window.print();
       } );
-    }
-  }
-})();
-
-
-/*
-  WP Comment Form
-*/
-(function() { 'use strict';
-
-  document.addEventListener( 'DOMContentLoaded', commentFormToggle );
-
-  /*
-    Open Comment Form and add Placeholder to the textarea
-  */
-  function commentFormToggle() {
-    // exit if comment form not found
-    if( document.querySelector('#reply-title') === null ) { return false; }
-
-    // Create placeholder's text based on Form Title.
-    let replyTitle = document.querySelector( '#reply-title' ).childNodes;
-    let replyTo = replyTitle[1].childNodes[0].nodeValue;
-    let placeholder = replyTitle[0].nodeValue + (replyTo ? replyTo : '') + '…';
-
-    // change the comment field's placeholder
-    document.querySelector('.comment-form textarea').setAttribute( 'placeholder', placeholder );
-    
-    // click listener to toggle the form
-    document.querySelector('.comment-form').addEventListener( 'click', activateForm );
-    
-    let $replyLinks = document.querySelectorAll( '.comment-reply-link' );
-    for( let $rl of $replyLinks ) {
-      $rl.addEventListener( 'click', activateForm );
-    }
-
-    //
-    function activateForm( e ) {
-      var $form = document.querySelector( '.comment-form' );
-      $form.classList.add( 'active' );
-      $form.removeEventListener( 'click' );
-      $form.querySelector( 'textarea' ).focus();
     }
   }
 })();
