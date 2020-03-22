76424220ded9629fded4d538a68a0b6d9b7333e5
Henner Setyono <henner.renardi@gmail.com>
2020-03-22 17:13:30 +0700

    Update edje-wp-library.php

--- edje-wp-library.php
+++ edje-wp-library.php
@@ -8,13 +8,13 @@
  * License: MIT
  * Author: Pixel Studio
  * Author URI: https://pixelstudio.id
- * Version: 4.0.0
+ * Version: 3.7.0
  */
 
 if( !defined( 'WPINC' ) ) { die; } // exit if accessed directly
 
 // Constant
-define( 'H_VERSION', '4.0.0' );
+define( 'H_VERSION', '3.7.0' );
 define( 'H_BASE', basename(dirname(__FILE__) ).'/'.basename(__FILE__) );
 
 define( 'H_DIR', __DIR__ ); // for require
