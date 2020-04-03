61ce6236e139e4f7c5357ec26ab5150d00874e4a
Henner S <henner.renardi@gmail.com>
2020-04-03 17:14:33 +0700

    3.7.3
    - [Vendor] Removed CSS dependency on Edje framework
    - [Vendor] Replaced some value with CSS variable that's defined by Custy.

--- module-vendor/_index.php
+++ module-vendor/_index.php
@@ -16,13 +16,14 @@ function _h_load_vendor() {
  * @action wp_enqueue_scripts
  */
 function _h_enqueue_vendor() {
-  $assets = plugin_dir_url(__FILE__) . 'assets';
+  $css = plugin_dir_url(__FILE__) . 'css';
+  $js = plugin_dir_url(__FILE__) . 'js';
 
   // Register H Library, you need to enqueue it in theme
-  wp_register_script( 'h-slider', $assets . '/h-slider.min.js', [], false, true );
-  wp_register_script( 'h-scroll', $assets . '/h-scroll.min.js', [], false, true );
-  wp_register_script( 'h-lightbox', $assets . '/h-lightbox.min.js', [], false, true );
+  wp_register_script( 'h-slider', $js . '/h-slider.min.js', [], false, true );
+  wp_register_script( 'h-scroll', $js . '/h-scroll.min.js', [], false, true );
+  wp_register_script( 'h-lightbox', $js . '/h-lightbox.min.js', [], false, true );
 
-  wp_register_style( 'h-slider', $assets . '/h-slider.css' );
-  wp_register_style( 'h-lightbox', $assets . '/h-lightbox.css' );
+  wp_register_style( 'h-slider', $css . '/h-slider.css' );
+  wp_register_style( 'h-lightbox', $css . '/h-lightbox.css' );
 }
