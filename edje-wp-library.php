3bdf5df07f61a80d78555b9cb49e1e82c98adcee
Henner S <henner.renardi@gmail.com>
2020-12-23 13:33:55 +0700

    4.1.8

--- edje-wp-library.php
+++ edje-wp-library.php
@@ -8,13 +8,13 @@
  * License: MIT
  * Author: Pixel Studio
  * Author URI: https://pixelstudio.id
- * Version: 4.1.7
+ * Version: 4.1.8
  */
 
 if( !defined( 'WPINC' ) ) { die; } // exit if accessed directly
 
 // Constant
-define( 'H_VERSION', '4.1.7' );
+define( 'H_VERSION', '4.1.8' );
 define( 'H_BASE', basename(dirname(__FILE__) ).'/'.basename(__FILE__) );
 
 define( 'H_DIR', __DIR__ ); // for require
